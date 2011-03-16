3df34c2657779221387610457d7f3ae29379e81b
minorg <devnull@localhost>
2011-03-16 15:15:32 -0400

    HTTP parser tests compiling again

--- test/yield/http/http_request_parser_test.py
+++ test/yield/http/http_request_parser_test.py
@@ -27,9 +27,8 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
 # THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-from yutil.constant import INDENT_SPACES
-from yutil.file import write_file
-from yutil.string import indent
+from yuild.constant import INDENT_SPACES
+from yutil import indent, write_file
 
 
 # Constants
@@ -49,69 +48,66 @@ TE_CHUNKED_FIELD = "Transfer-Encoding: chunked" + CRLF
 BODY2 = "12"
 
 # Assertions
-ASSERT_1_BODY_NONNULL = "throw_assert_ne( http_request->get_body(), NULL );"
-ASSERT_1_BODY_NULL = "throw_assert_eq( http_request->get_body(), NULL );"
-ASSERT_1_BODY2 = "throw_assert_eq( http_request->get_content_length(), 2 );"
-ASSERT_1_HOST_FIELD = """throw_assert_eq( ( *http_request )["Host"], "%(HOST)s" );""" % locals()
-ASSERT_1_METHOD = """throw_assert_eq( http_request->get_method(), HTTPRequest::METHOD_GET );""" % locals()
-ASSERT_1_NONNULL = "throw_assert_ne( http_request, NULL );"
-ASSERT_1_NULL = "throw_assert_eq( http_request, NULL );"
-ASSERT_1_URI = """throw_assert_eq( strcmp( http_request->get_uri(), "%(URI)s" ), 0 );""" % locals()
-ASSERT_1_VERSION = "throw_assert_eq( http_request->get_http_version(), 1.1F );"
+ASSERT_1_BODY_NONNULL = "throw_assert_ne(http_request->get_body(), NULL);"
+ASSERT_1_BODY_NULL = "throw_assert_eq(http_request->get_body(), NULL);"
+ASSERT_1_BODY2 = "throw_assert_eq(http_request->get_content_length(), 2);"
+ASSERT_1_HOST_FIELD = """throw_assert_eq((*http_request)["Host"], "%(HOST)s");""" % locals()
+ASSERT_1_METHOD = """throw_assert_eq(http_request->get_method(), HTTPRequest::METHOD_GET);""" % locals()
+ASSERT_1_NONNULL = "throw_assert_ne(http_request, NULL);"
+ASSERT_1_NULL = "throw_assert_eq(http_request, NULL);"
+ASSERT_1_URI = """throw_assert_eq(strcmp(http_request->get_uri(), "%(URI)s"), 0);""" % locals()
+ASSERT_1_VERSION = "throw_assert_eq(http_request->get_http_version(), 1.1F);"
 
-def ASSERT_N( n ):
-    return "throw_assert_eq( http_requests.size(), %(n)u );" % locals()
-def ASSERT_N_BODY_NONNULL( n ):
-    return """for ( size_t i = 0; i < %(n)u; i++ ) { throw_assert_ne( http_requests[i]->get_body(), NULL ); }""" % locals()
-def ASSERT_N_BODY_NULL( n ):
-    return """for ( size_t i = 0; i < %(n)u; i++ ) { throw_assert_eq( http_requests[i]->get_body(), NULL ); }""" % locals()
-def ASSERT_N_BODY2( n ):
-    return "for ( size_t i = 0; i < %(n)u; i++ ) { throw_assert_eq( http_requests[i]->get_content_length(), 2 ); }" % locals()
-def ASSERT_N_METHOD( n ):
-    return """for ( size_t i = 0; i < %(n)u; i++ ) { throw_assert_eq( http_requests[i]->get_method(), HTTPRequest::METHOD_GET ); }""" % locals()
-def ASSERT_N_URI( n ):
+def ASSERT_N(n):
+    return "throw_assert_eq(http_requests.size(), %(n)u);" % locals()
+def ASSERT_N_BODY_NONNULL(n):
+    return """for (size_t i = 0; i < %(n)u; i++) { throw_assert_ne(http_requests[i]->get_body(), NULL); }""" % locals()
+def ASSERT_N_BODY_NULL(n):
+    return """for (size_t i = 0; i < %(n)u; i++) { throw_assert_eq(http_requests[i]->get_body(), NULL); }""" % locals()
+def ASSERT_N_BODY2(n):
+    return "for (size_t i = 0; i < %(n)u; i++) { throw_assert_eq(http_requests[i]->get_content_length(), 2); }" % locals()
+def ASSERT_N_METHOD(n):
+    return """for (size_t i = 0; i < %(n)u; i++) { throw_assert_eq(http_requests[i]->get_method(), HTTPRequest::METHOD_GET); }""" % locals()
+def ASSERT_N_URI(n):
     URI = globals()["URI"]
-    return """for ( size_t i = 0; i < %(n)u; i++ ) { throw_assert_eq( strcmp( http_requests[i]->get_uri(), "%(URI)s" ), 0 ); }""" % locals()
-def ASSERT_N_VERSION( n ):
-    return "for ( size_t i = 0; i < %(n)u; i++ ) { throw_assert_eq( http_requests[i]->get_http_version(), 1.1F ); }" % locals()
-def ASSERT_N_HOST_FIELD( n ):
+    return """for (size_t i = 0; i < %(n)u; i++) { throw_assert_eq(strcmp(http_requests[i]->get_uri(), "%(URI)s"), 0); }""" % locals()
+def ASSERT_N_VERSION(n):
+    return "for (size_t i = 0; i < %(n)u; i++) { throw_assert_eq(http_requests[i]->get_http_version(), 1.1F); }" % locals()
+def ASSERT_N_HOST_FIELD(n):
     HOST = globals()["HOST"]
-    return """for ( size_t i = 0; i < %(n)u; i++ ) { throw_assert_eq( ( *http_requests[i] )["Host"], "%(HOST)s" ); }""" % locals()
+    return """for (size_t i = 0; i < %(n)u; i++) { throw_assert_eq((*http_requests[i])["Host"], "%(HOST)s"); }""" % locals()
 
 # Read calls
-def PARSE_1( *args ):
-    args = "".join( args )
-    return """HTTPRequest* http_request = object_cast<HTTPRequest>( HTTPRequestParser( \"%(args)s\" ).parse() );""" % locals()
+def PARSE_1(*args):
+    args = "".join(args)
+    return """HTTPRequest* http_request = object_cast<HTTPRequest>(HTTPRequestParser(\"%(args)s\").parse());""" % locals()
 
-def PARSE_N( n, *args ):
-    args = "".join( args )
+def PARSE_N(n, *args):
+    args = "".join(args)
     return """\
-HTTPRequestParser http_request_parser( \"%(args)s\" );
+HTTPRequestParser http_request_parser(\"%(args)s\");
 vector<HTTPRequest*> http_requests;
 
-for ( uint32_t i = 0; i < %(n)u; i++ )
-{
-  HTTPRequest* http_request = object_cast<HTTPRequest>( http_request_parser.parse() );
+for (uint32_t i = 0; i < %(n)u; i++) {
+  HTTPRequest* http_request = object_cast<HTTPRequest>(http_request_parser.parse());
 
-  if ( http_request != NULL )
-    http_requests.push_back( http_request );
-  else
-  {
-    while ( !http_requests.empty() )
-    {
-      HTTPRequest::dec_ref( *http_requests.back() );
+  if (http_request != NULL)
+    http_requests.push_back(http_request);
+  else {
+    while (!http_requests.empty()) {
+      HTTPRequest::dec_ref(*http_requests.back());
       http_requests.pop_back();
     }
 
-    throw_assert( false );
+    throw_assert(false);
   }
 }
 """ % locals()
 
 # dec_ref calls
-DEC_REF_1 = "HTTPRequest::dec_ref( *http_request );"
-def DEC_REF_N( n ):
-    return "for ( size_t i = 0; i < %(n)u; i++ ) HTTPRequest::dec_ref( *http_requests[i] );" % locals()
+DEC_REF_1 = "HTTPRequest::dec_ref(*http_request);"
+def DEC_REF_N(n):
+    return "for (size_t i = 0; i < %(n)u; i++) HTTPRequest::dec_ref(*http_requests[i]);" % locals()
 
 
 
@@ -121,43 +117,43 @@ test_cases = {}
 
 test_cases["malformed_method_missing"] = \
 (
-    PARSE_1( URI, " HTTP/1.0", CRLF, HOST_FIELD, CRLF ),
+    PARSE_1(URI, " HTTP/1.0", CRLF, HOST_FIELD, CRLF),
     ASSERT_1_NULL
 )
 
 test_cases["malformed_uri_embedded_lf"] = \
 (
-    PARSE_1( "GET", " /\\r ", VERSION, CRLF, HOST_FIELD, CRLF ),
+    PARSE_1("GET", " /\\r ", VERSION, CRLF, HOST_FIELD, CRLF),
     ASSERT_1_NULL
 )
 
 test_cases["malformed_uri_missing"] = \
 (
-    PARSE_1( "GET", ' ', VERSION, CRLF, HOST_FIELD, CRLF ),
+    PARSE_1("GET", ' ', VERSION, CRLF, HOST_FIELD, CRLF),
     ASSERT_1_NULL
 )
 
 test_cases["malformed_version_missing"] = \
 (
-    PARSE_1( "GET", ' ', URI, CRLF, HOST_FIELD, CRLF ),
+    PARSE_1("GET", ' ', URI, CRLF, HOST_FIELD, CRLF),
     ASSERT_1_NULL
 )
 
 test_cases["malformed_version_missing_http"] = \
 (
-    PARSE_1( "GET", ' ', URI, ' ', "/1.0", CRLF, HOST_FIELD, CRLF ),
+    PARSE_1("GET", ' ', URI, ' ', "/1.0", CRLF, HOST_FIELD, CRLF),
     ASSERT_1_NULL
 )
 
 test_cases["malformed_version_missing_minor_version"] = \
 (
-    PARSE_1( "GET", ' ', URI, " HTTP/1.", CRLF, HOST_FIELD, CRLF ),
+    PARSE_1("GET", ' ', URI, " HTTP/1.", CRLF, HOST_FIELD, CRLF),
     ASSERT_1_NULL
 )
 
 test_cases["malformed_version_missing_trailing_crlf"] = \
 (
-    PARSE_1( "GET", ' ', URI, ' ', VERSION, HOST_FIELD, CRLF ),
+    PARSE_1("GET", ' ', URI, ' ', VERSION, HOST_FIELD, CRLF),
     ASSERT_1_NULL
 )
 
@@ -170,16 +166,16 @@ test_cases["well_formed_chunk_1_body"] = \
         HOST_FIELD,
         TE_CHUNKED_FIELD,
         CRLF,
-        "1" + CRLF + "x" + CRLF + "0" + CRLF*2
-    ),
-    ASSERT_N( 2 ),
-    ASSERT_N_METHOD( 2 ),
-    # ASSERT_N_URI( 2 ),
-    ASSERT_N_VERSION( 2 ),
-    ASSERT_N_BODY_NONNULL( 2 ),
-    "throw_assert_eq( http_requests[0]->get_content_length(), 1 );",
-    "throw_assert_eq( http_requests[1]->get_content_length(), 0 );",
-    DEC_REF_N( 2 )
+        "1" + CRLF + "x" + CRLF + "0" + CRLF * 2
+   ),
+    ASSERT_N(2),
+    ASSERT_N_METHOD(2),
+    # ASSERT_N_URI(2),
+    ASSERT_N_VERSION(2),
+    ASSERT_N_BODY_NONNULL(2),
+    "throw_assert_eq(http_requests[0]->get_content_length(), 1);",
+    "throw_assert_eq(http_requests[1]->get_content_length(), 0);",
+    DEC_REF_N(2)
 )
 
 test_cases["well_formed_chunk_2_body"] = \
@@ -191,23 +187,23 @@ test_cases["well_formed_chunk_2_body"] = \
         HOST_FIELD,
         TE_CHUNKED_FIELD,
         CRLF,
-        "1" + CRLF + "x" + CRLF + "1" + CRLF + "y" + CRLF + "0" + CRLF*2
-    ),
-    ASSERT_N( 3 ),
-    ASSERT_N_METHOD( 3 ),
-    # ASSERT_N_URI( 3 ),
-    ASSERT_N_VERSION( 3 ),
-    ASSERT_N_HOST_FIELD( 3 ),
-    ASSERT_N_BODY_NONNULL( 3 ),
-    "throw_assert_eq( http_requests[0]->get_content_length(), 1 );",
-    "throw_assert_eq( http_requests[1]->get_content_length(), 1 );",
-    "throw_assert_eq( http_requests[2]->get_content_length(), 0 );",
-    DEC_REF_N( 3 )
+        "1" + CRLF + "x" + CRLF + "1" + CRLF + "y" + CRLF + "0" + CRLF * 2
+   ),
+    ASSERT_N(3),
+    ASSERT_N_METHOD(3),
+    # ASSERT_N_URI(3),
+    ASSERT_N_VERSION(3),
+    ASSERT_N_HOST_FIELD(3),
+    ASSERT_N_BODY_NONNULL(3),
+    "throw_assert_eq(http_requests[0]->get_content_length(), 1);",
+    "throw_assert_eq(http_requests[1]->get_content_length(), 1);",
+    "throw_assert_eq(http_requests[2]->get_content_length(), 0);",
+    DEC_REF_N(3)
 )
 
 test_cases["well_formed_no_body"] = \
 (
-    PARSE_1( "GET", ' ', URI, ' ', VERSION, CRLF, HOST_FIELD, CRLF ),
+    PARSE_1("GET", ' ', URI, ' ', VERSION, CRLF, HOST_FIELD, CRLF),
     ASSERT_1_NONNULL,
     ASSERT_1_METHOD,
     ASSERT_1_VERSION,
@@ -218,7 +214,7 @@ test_cases["well_formed_no_body"] = \
 
 test_cases["well_formed_no_fields"] = \
 (
-    PARSE_1( "GET", ' ', URI, ' ', VERSION, CRLF, CRLF ),
+    PARSE_1("GET", ' ', URI, ' ', VERSION, CRLF, CRLF),
     ASSERT_1_NONNULL,
     ASSERT_1_VERSION,
     ASSERT_1_BODY_NULL,
@@ -227,7 +223,7 @@ test_cases["well_formed_no_fields"] = \
 
 test_cases["well_formed_normal_body"] = \
 (
-    PARSE_1( "GET", ' ', URI, ' ', VERSION, CRLF, HOST_FIELD, CL2_FIELD, CRLF, BODY2 ),
+    PARSE_1("GET", ' ', URI, ' ', VERSION, CRLF, HOST_FIELD, CL2_FIELD, CRLF, BODY2),
     ASSERT_1_NONNULL,
     ASSERT_1_BODY_NONNULL,
     ASSERT_1_BODY2,
@@ -242,14 +238,14 @@ test_cases["well_formed_pipelined_no_body"] = \
         2,
         "GET", ' ', URI, ' ', VERSION, CRLF, HOST_FIELD, CRLF,
         "GET", ' ', URI, ' ', VERSION, CRLF, HOST_FIELD, CRLF,
-    ),
-    ASSERT_N( 2 ),
-    ASSERT_N_METHOD( 2 ),
-    # ASSERT_N_URI( 2 ),
-    ASSERT_N_VERSION( 2 ),
-    ASSERT_N_HOST_FIELD( 2 ),
-    ASSERT_N_BODY_NULL( 2 ),
-    DEC_REF_N( 2 )
+   ),
+    ASSERT_N(2),
+    ASSERT_N_METHOD(2),
+    # ASSERT_N_URI(2),
+    ASSERT_N_VERSION(2),
+    ASSERT_N_HOST_FIELD(2),
+    ASSERT_N_BODY_NULL(2),
+    DEC_REF_N(2)
 )
 
 test_cases["well_formed_pipelined_normal_body"] = \
@@ -259,15 +255,15 @@ test_cases["well_formed_pipelined_normal_body"] = \
         2,
         "GET", ' ', URI, ' ', VERSION, CRLF, HOST_FIELD, CL2_FIELD, CRLF, BODY2,
         "GET", ' ', URI, ' ', VERSION, CRLF, HOST_FIELD, CL2_FIELD, CRLF, BODY2,
-    ),
-    ASSERT_N( 2 ),
-    ASSERT_N_METHOD( 2 ),
-    # ASSERT_N_URI( 2 ),
-    ASSERT_N_VERSION( 2 ),
-    ASSERT_N_HOST_FIELD( 2 ),
-    ASSERT_N_BODY_NONNULL( 2 ),
-    ASSERT_N_BODY2( 2 ),
-    DEC_REF_N( 2 )
+   ),
+    ASSERT_N(2),
+    ASSERT_N_METHOD(2),
+    # ASSERT_N_URI(2),
+    ASSERT_N_VERSION(2),
+    ASSERT_N_HOST_FIELD(2),
+    ASSERT_N_BODY_NONNULL(2),
+    ASSERT_N_BODY2(2),
+    DEC_REF_N(2)
 )
 
 
@@ -275,20 +271,18 @@ test_case_names = test_cases.keys()
 test_case_names.sort()
 test_case_names.reverse()
 for test_case_name in test_case_names:
-    test_case = '\n'.join( test_cases[test_case_name] )
-    test_case = indent( INDENT_SPACES, test_case )
+    test_case = '\n'.join(test_cases[test_case_name])
+    test_case = indent(INDENT_SPACES, test_case)
     test_cases[test_case_name] = """\
-TEST( HTTPRequestParser, %(test_case_name)s )
-{
+TEST(HTTPRequestParser, %(test_case_name)s) {
 %(test_case)s
 }
 """ % locals()
-test_cases =\
+test_cases = \
     '\n'.join(
         [test_cases[test_case_name]
         for test_case_name in test_case_names]
-    )
-test_cases = indent( INDENT_SPACES * 2, test_cases )
+   )
 
 write_file(
     "http_request_parser_test.cpp",
@@ -300,14 +294,11 @@ write_file(
 #include "yield/http/http_request_parser.hpp"
 #include "yunit.hpp"
 
+TEST_SUITE(HTTPRequestParser);
 
-TEST_SUITE( HTTPRequestParser );
-
-namespace yield
-{
-  namespace http
-  {
+namespace yield {
+namespace http {
 %(test_cases)s
-  }
 }
-""" % locals() )
+}
+""" % locals())
--- test/yield/http/http_response_parser_test.py
+++ test/yield/http/http_response_parser_test.py
@@ -27,9 +27,8 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
 # THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-from yutil.constant import INDENT_SPACES
-from yutil.file import write_file
-from yutil.string import indent
+from yuild.constant import INDENT_SPACES
+from yutil import indent, write_file
 
 
 # Constants
@@ -49,64 +48,61 @@ DATE = "Fri, 31 Dec 1999 23:59:59 GMT"
 DATE_FIELD = "Date: %(DATE)s" % locals() + CRLF
 
 # Assertions
-ASSERT_1_BODY_NONNULL = "throw_assert_ne( http_response->get_body(), NULL );"
-ASSERT_1_BODY_NULL = "throw_assert_eq( http_response->get_body(), NULL );"
-ASSERT_1_BODY2 = "throw_assert_eq( http_response->get_content_length(), 2 );"
-ASSERT_1_DATE_FIELD = """throw_assert_eq( ( *http_response )["Date"], "%(DATE)s" );""" % locals()
-ASSERT_1_NONNULL = "throw_assert_ne( http_response, NULL );"
-ASSERT_1_NULL = "throw_assert_eq( http_response, NULL );"
-def ASSERT_1_STATUS_CODE( status_code ):
-    return "throw_assert_eq( http_response->get_status_code(), %(status_code)u );" % locals()
+ASSERT_1_BODY_NONNULL = "throw_assert_ne(http_response->get_body(), NULL);"
+ASSERT_1_BODY_NULL = "throw_assert_eq(http_response->get_body(), NULL);"
+ASSERT_1_BODY2 = "throw_assert_eq(http_response->get_content_length(), 2);"
+ASSERT_1_DATE_FIELD = """throw_assert_eq((*http_response)["Date"], "%(DATE)s");""" % locals()
+ASSERT_1_NONNULL = "throw_assert_ne(http_response, NULL);"
+ASSERT_1_NULL = "throw_assert_eq(http_response, NULL);"
+def ASSERT_1_STATUS_CODE(status_code):
+    return "throw_assert_eq(http_response->get_status_code(), %(status_code)u);" % locals()
 
-def ASSERT_N( n ):
-    return "throw_assert_eq( http_responses.size(), %(n)u );" % locals()
-def ASSERT_N_BODY_NONNULL( n ):
-    return """for ( size_t i = 0; i < %(n)u; i++ ) { throw_assert_ne( http_responses[i]->get_body(), NULL ); }""" % locals()
-def ASSERT_N_BODY_NULL( n ):
-    return """for ( size_t i = 0; i < %(n)u; i++ ) { throw_assert_eq( http_responses[i]->get_body(), NULL ); }""" % locals()
-def ASSERT_N_BODY2( n ):
-    return "for ( size_t i = 0; i < %(n)u; i++ ) { throw_assert_eq( http_responses[i]->get_content_length(), 2 ); }" % locals()
-def ASSERT_N_DATE_FIELD( n ):
+def ASSERT_N(n):
+    return "throw_assert_eq(http_responses.size(), %(n)u);" % locals()
+def ASSERT_N_BODY_NONNULL(n):
+    return """for (size_t i = 0; i < %(n)u; i++) { throw_assert_ne(http_responses[i]->get_body(), NULL); }""" % locals()
+def ASSERT_N_BODY_NULL(n):
+    return """for (size_t i = 0; i < %(n)u; i++) { throw_assert_eq(http_responses[i]->get_body(), NULL); }""" % locals()
+def ASSERT_N_BODY2(n):
+    return "for (size_t i = 0; i < %(n)u; i++) { throw_assert_eq(http_responses[i]->get_content_length(), 2); }" % locals()
+def ASSERT_N_DATE_FIELD(n):
     DATE = globals()["DATE"]
-    return """for ( size_t i = 0; i < %(n)u; i++ ) { throw_assert_eq( ( *http_responses[i] )["Date"], "%(DATE)s" ); }""" % locals()
-def ASSERT_N_STATUS_CODE( n, status_code ):
-    return "for ( size_t i = 0; i < %(n)u; i++ ) { throw_assert_eq( http_responses[i]->get_status_code(), %(status_code)u ); }" % locals()
+    return """for (size_t i = 0; i < %(n)u; i++) { throw_assert_eq((*http_responses[i])["Date"], "%(DATE)s"); }""" % locals()
+def ASSERT_N_STATUS_CODE(n, status_code):
+    return "for (size_t i = 0; i < %(n)u; i++) { throw_assert_eq(http_responses[i]->get_status_code(), %(status_code)u); }" % locals()
 
 # Read calls
-def PARSE_1( *args ):
-    args = "".join( args )
-    return """HTTPResponse* http_response = object_cast<HTTPResponse>( HTTPResponseParser( \"%(args)s\" ).parse() );""" % locals()
+def PARSE_1(*args):
+    args = "".join(args)
+    return """HTTPResponse* http_response = object_cast<HTTPResponse>(HTTPResponseParser(\"%(args)s\").parse());""" % locals()
 
-def PARSE_N( n, *args ):
-    args = "".join( args )
+def PARSE_N(n, *args):
+    args = "".join(args)
     return """\
-HTTPResponseParser http_response_parser( \"%(args)s\" );
+HTTPResponseParser http_response_parser(\"%(args)s\");
 vector<HTTPResponse*> http_responses;
 
-for ( uint32_t i = 0; i < %(n)u; i++ )
-{
-  HTTPResponse* http_response = object_cast<HTTPResponse>( http_response_parser.parse() );
+for (uint32_t i = 0; i < %(n)u; i++) {
+  HTTPResponse* http_response = object_cast<HTTPResponse>(http_response_parser.parse());
 
-  if ( http_response != NULL )
-    http_responses.push_back( http_response );
-  else
-  {
-    while ( !http_responses.empty() )
-    {
-      HTTPResponse::dec_ref( *http_responses.back() );
+  if (http_response != NULL)
+    http_responses.push_back(http_response);
+  else {
+    while (!http_responses.empty()) {
+      HTTPResponse::dec_ref(*http_responses.back());
       http_responses.pop_back();
     }
 
-    throw_assert( false );
+    throw_assert(false);
   }
 }
 """ % locals()
 
 
 # dec_ref calls
-DEC_REF_1 = "HTTPResponse::dec_ref( *http_response );"
-def DEC_REF_N( n ):
-    return "for ( size_t i = 0; i < %(n)u; i++ ) HTTPResponse::dec_ref( *http_responses[i] );" % locals()
+DEC_REF_1 = "HTTPResponse::dec_ref(*http_response);"
+def DEC_REF_N(n):
+    return "for (size_t i = 0; i < %(n)u; i++) HTTPResponse::dec_ref(*http_responses[i]);" % locals()
 
 
 
@@ -116,33 +112,33 @@ test_cases = {}
 
 test_cases["malformed_reason_phrase_missing"] = \
 (
-    PARSE_1( VERSION, " 200", CRLF, DATE_FIELD, CRLF ),
+    PARSE_1(VERSION, " 200", CRLF, DATE_FIELD, CRLF),
     ASSERT_1_NULL
 )
 
 test_cases["malformed_status_code_alpha"] = \
 (
-    PARSE_1( VERSION, " XX OK", CRLF, DATE_FIELD, CRLF ),
+    PARSE_1(VERSION, " XX OK", CRLF, DATE_FIELD, CRLF),
     ASSERT_1_NULL
 )
 
 test_cases["malformed_status_code_missing"] = \
 (
-    PARSE_1( VERSION, " OK", CRLF, DATE_FIELD, CRLF ),
+    PARSE_1(VERSION, " OK", CRLF, DATE_FIELD, CRLF),
     ASSERT_1_NULL
 )
 
 test_cases["malformed_status_line_missing"] = \
 (
-    PARSE_1( DATE_FIELD, CRLF ),
+    PARSE_1(DATE_FIELD, CRLF),
     ASSERT_1_NULL
 )
 
 test_cases["well_formed_no_body"] = \
 (
-    PARSE_1( VERSION, ' ', OK, CRLF, DATE_FIELD, CRLF ),
+    PARSE_1(VERSION, ' ', OK, CRLF, DATE_FIELD, CRLF),
     ASSERT_1_NONNULL,
-    ASSERT_1_STATUS_CODE( 200 ),
+    ASSERT_1_STATUS_CODE(200),
     ASSERT_1_DATE_FIELD,
     ASSERT_1_BODY_NULL,
     DEC_REF_1
@@ -150,9 +146,9 @@ test_cases["well_formed_no_body"] = \
 
 test_cases["well_formed_normal_body"] = \
 (
-    PARSE_1( VERSION, ' ', OK, CRLF, DATE_FIELD, CL2_FIELD, CRLF, BODY2 ),
+    PARSE_1(VERSION, ' ', OK, CRLF, DATE_FIELD, CL2_FIELD, CRLF, BODY2),
     ASSERT_1_NONNULL,
-    ASSERT_1_STATUS_CODE( 200 ),
+    ASSERT_1_STATUS_CODE(200),
     ASSERT_1_DATE_FIELD,
     ASSERT_1_BODY_NONNULL,
     ASSERT_1_BODY2,
@@ -166,12 +162,12 @@ test_cases["well_formed_pipelined_no_body"] = \
         2,
         VERSION, ' ', OK, CRLF, DATE_FIELD, CRLF,
         VERSION, ' ', OK, CRLF, DATE_FIELD, CRLF,
-    ),
-    ASSERT_N( 2 ),
-    ASSERT_N_STATUS_CODE( 2, 200 ),
-    ASSERT_N_DATE_FIELD( 2 ),
-    ASSERT_N_BODY_NULL( 2 ),
-    DEC_REF_N( 2 )
+   ),
+    ASSERT_N(2),
+    ASSERT_N_STATUS_CODE(2, 200),
+    ASSERT_N_DATE_FIELD(2),
+    ASSERT_N_BODY_NULL(2),
+    DEC_REF_N(2)
 )
 
 test_cases["well_formed_pipelined_normal_body"] = \
@@ -181,20 +177,20 @@ test_cases["well_formed_pipelined_normal_body"] = \
         2,
         VERSION, ' ', OK, CRLF, DATE_FIELD, CL2_FIELD, CRLF, BODY2,
         VERSION, ' ', OK, CRLF, DATE_FIELD, CL2_FIELD, CRLF, BODY2,
-    ),
-    ASSERT_N( 2 ),
-    ASSERT_N_STATUS_CODE( 2, 200 ),
-    ASSERT_N_DATE_FIELD( 2 ),
-    ASSERT_N_BODY_NONNULL( 2 ),
-    ASSERT_N_BODY2( 2 ),
-    DEC_REF_N( 2 )
+   ),
+    ASSERT_N(2),
+    ASSERT_N_STATUS_CODE(2, 200),
+    ASSERT_N_DATE_FIELD(2),
+    ASSERT_N_BODY_NONNULL(2),
+    ASSERT_N_BODY2(2),
+    DEC_REF_N(2)
 )
 
 test_cases["well_formed_reason_phrase_split"] = \
 (
-    PARSE_1( VERSION, ' ', "415 Unsupported Media Type", CRLF, DATE_FIELD, CRLF ),
+    PARSE_1(VERSION, ' ', "415 Unsupported Media Type", CRLF, DATE_FIELD, CRLF),
     ASSERT_1_NONNULL,
-    ASSERT_1_STATUS_CODE( 415 ),
+    ASSERT_1_STATUS_CODE(415),
     ASSERT_1_DATE_FIELD,
     ASSERT_1_BODY_NULL,
     DEC_REF_1
@@ -205,20 +201,18 @@ test_case_names = test_cases.keys()
 test_case_names.sort()
 test_case_names.reverse()
 for test_case_name in test_case_names:
-    test_case = '\n'.join( test_cases[test_case_name] )
-    test_case = indent( INDENT_SPACES, test_case )
+    test_case = '\n'.join(test_cases[test_case_name])
+    test_case = indent(INDENT_SPACES, test_case)
     test_cases[test_case_name] = """\
-TEST( HTTPResponseParser, %(test_case_name)s )
-{
+TEST(HTTPResponseParser, %(test_case_name)s) {
 %(test_case)s
 }
 """ % locals()
-test_cases =\
+test_cases = \
     '\n'.join(
         [test_cases[test_case_name]
         for test_case_name in test_case_names]
-    )
-test_cases = indent( INDENT_SPACES * 2, test_cases )
+   )
 
 
 write_file(
@@ -231,14 +225,11 @@ write_file(
 #include "yield/http/http_response_parser.hpp"
 #include "yunit.hpp"
 
+TEST_SUITE(HTTPResponseParser);
 
-TEST_SUITE( HTTPResponseParser );
-
-namespace yield
-{
-  namespace http
-  {
+namespace yield {
+namespace http {
 %(test_cases)s
-  }
 }
-""" % locals() )
+}
+""" % locals())
