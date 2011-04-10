a301dd32d0f47ba5b0c20da492369f2f2fa68b38
minorg <devnull@localhost>
2011-04-10 16:17:42 -0400

    fs.poll: working on inotify

--- src/yield/fs/poll/linux/fs_event_queue.cpp
+++ src/yield/fs/poll/linux/fs_event_queue.cpp
@@ -166,33 +166,14 @@ public:
     return wd;
   }
 
-  void read(NonBlockingConcurrentQueue<FSEvent, 1024>& fs_events) {
-    uint32_t mask, cookie;
-    Path name;
-    if (last_read_mask == 0) {
-      uint32_t mask_cookie_len[3];
-      ssize_t read_ret
-        = ::read(inotify_fd, mask_cookie_len, sizeof(mask_cookie_len));
-      debug_assert_eq(read_ret, static_cast<ssize_t>(sizeof(mask_cookie_len)));
-
-      mask = mask_cookie_len[0];
-      cookie = mask_cookie_len[1];
-      uint32_t len = mask_cookie_len[2];
-
-      if (len > 0) {
-        char* read_name = new char[len];
-        read_ret = ::read(inotify_fd, read_name, len);
-        debug_assert_eq(read_ret, static_cast<ssize_t>(len));
-        name = Path(read_name, len);
-        delete [] read_name;
-      } else
-        name = this->name;
-    } else {
-      mask = last_read_mask;
-      cookie = last_read_cookie;
-      name = last_read_name;
-    }
-
+  void
+  read(
+    const inotify_event& inotify_event_,
+    OUT NonBlockingConcurrentQueue<FSEvent, 1024>& fs_events
+  ) {
+    uint32_t cookie = inotify_event_.cookie;
+    uint32_t mask = inotify_event_.mask;
+    Path name = Path(inotify_event_.name, inotify_event_.len);
     bool isdir = (mask & IN_ISDIR) == IN_ISDIR;
     Path path = path / name;
 
@@ -270,33 +251,19 @@ public:
         if ((events & type) == type) {
           FSEvent* fs_event
             = new FSEvent(path / old_name_i->second, path, type);
-          fs_events.enqueue(*fs_event);
           old_names.erase(old_name_i);
-          //return true;
-          return;
+          fs_events.enqueue(*fs_event);
         } else {
           old_names.erase(old_name_i);
           continue;
         }
 
         mask ^= IN_MOVED_TO;
-      } else {
-        last_read_cookie = 0;
-        last_read_mask = 0;
-        //return false;
-        return;
-      }
-
-      if ((events & type) == type) {
-        FSEvent* fs_event = new FSEvent(path, type);
-        fs_events.enqueue(*fs_event);
-        last_read_cookie = cookie;
-        last_read_mask = mask;
-        last_read_name = name;
-        //return true;
-        return;
       } else
-        continue;
+        return NULL;
+
+      if ((events & type) == type)
+        fs_events.enqueue(*new FSEvent(path, type));
     }
   }
 
@@ -331,8 +298,6 @@ private:
 private:
   FSEvent::Type events;
   int inotify_fd;
-  uint32_t last_read_cookie, last_read_mask;
-  Path last_read_name;
   Path name;
   map<uint32_t, Path> old_names;
   Path path;
@@ -397,20 +362,33 @@ YO_NEW_REF Event* FSEventQueue::dequeue(const Time& timeout) {
       if (event->get_type_id() == FDEvent::TYPE_ID) {
         FDEvent* fd_event = static_cast<FDEvent*>(event);
         if (fd_event->get_fd() == inotify_fd) {
-          int wd;
-          ssize_t read_ret = ::read(inotify_fd, &wd, sizeof(wd));
-          debug_assert_eq(read_ret, static_cast<ssize_t>(sizeof(wd)));
+          char inotify_events[(sizeof(inotify_event_) + PATH_MAX) * 16];
+          ssize_t read_ret
+            = ::read(inotify_fd, inotify_events, sizeof(inotify_events));
 
-          for (
-            map<Path, Watch*>::iterator watch_i = watches.begin();
-            watch_i != watches.end();
-            ++watch_i
-          ) {
-            Watch* watch = watch_i->second->find_watch(wd);
-            if (watch != NULL) {
-              watch->read(fs_events);
-              return fs_events.trydequeue();
-            }
+          if (read_ret > 0) {
+            const char* inotify_events_p = inotify_events;
+            const char* inotify_events_pe
+              = inotify_events + static_cast<size_t>(read_ret);
+
+            do {
+              const inotify_event inotify_event_*
+                = reinterpret_cast<const inotify_event*>(inotify_events_p);
+
+              for (
+                map<Path, Watch*>::iterator watch_i = watches.begin();
+                watch_i != watches.end();
+                ++watch_i
+              ) {
+                Watch* watch = watch_i->second->find_watch(inotify_event_->wd);
+                if (watch != NULL)
+                  watch->read(*inotify_event_, fs_events);
+              }
+
+              inotify_events_p += sizeof(inotify_event) + inotify_event_->len;
+            } while (inotify_events_p < inotify_events_pe);
+
+            return fs_events.trydequeue();
           }
         } else
           return fd_event;
