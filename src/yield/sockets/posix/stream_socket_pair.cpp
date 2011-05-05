78d537e4ef12bd3bc286760b8472ea0c3f399f51
minorg <devnull@localhost>
2011-05-05 17:32:07 -0400

    sockets: appeasing gcc

--- src/yield/sockets/posix/stream_socket_pair.cpp
+++ src/yield/sockets/posix/stream_socket_pair.cpp
@@ -27,7 +27,7 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
 // THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#include "yield/sockets/datagram_socket_pair.hpp"
+#include "yield/sockets/stream_socket_pair.hpp"
 
 #include <sys/socket.h>
 
