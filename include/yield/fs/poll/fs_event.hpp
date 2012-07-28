41e41bf9f243e064ff8b3c9a387e3f66f0348b29
Minor Gordon <github@minorgordon.net>
2012-07-28 17:48:33 +0200

    appeasing gcc

--- include/yield/fs/poll/fs_event.hpp
+++ include/yield/fs/poll/fs_event.hpp
@@ -42,7 +42,7 @@ namespace poll {
 */
 class FSEvent  : public Event {
 public:
-  const static uint32_t FSEvent::TYPE_ID = 1640119324;
+  const static uint32_t TYPE_ID = 1640119324;
 
 public:
   /**
